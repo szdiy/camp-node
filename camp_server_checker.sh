1bc090faeb2f13cb5d237f8ab291f3ddd983885c
Nala Ginrut <nalaginrut@gmail.com>
2015-07-07 19:48:15 +0800

    fixed to start cont table correctly

--- camp_server_checker.sh
+++ camp_server_checker.sh
@@ -2,7 +2,6 @@
 
 pid=`ps aux | grep guile | grep vd.scm | awk '{print $2}'`
 
-if [[ -z pid ]]; then
-   echo "vd.scm need to start!"
+[[ -z $pid ]] &&
+   echo "vd.scm need to start!" &&
    exec guile /var/www/vd.scm 1> /var/www/vd.log 2> /var/www/vd.err &
-fi
--- node_campus_check.sh
+++ node_campus_check.sh
@@ -2,8 +2,8 @@
 
 pid=`ps aux | grep python | grep vd.py | awk '{print $2}'`
 
-if [[ -z pid ]]; then
-   echo "need to start!"
-   cd /home/pi/camp-node/
+[[ -z $pid ]] &&
+   echo "need to start!" &&
+   cd /home/pi/camp-node/ &&
    python vd.py 1> vd.log 2> vd.err &
-fi
+
