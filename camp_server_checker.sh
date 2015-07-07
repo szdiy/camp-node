6f8e3b7011aa8bfa5f6e684b07c47037024bec88
Nala Ginrut <nalaginrut@gmail.com>
2015-07-07 18:54:06 +0800

    fixed to check pid correctly

--- camp_server_checker.sh
+++ camp_server_checker.sh
@@ -2,6 +2,6 @@
 
 pid=`ps aux | grep guile | grep vd.scm | awk '{print $2}'`
 
-if [[ !pid ]]; then
+if [[ -z pid ]]; then
    exec guile /var/www/vd.scm 1> /var/www/vd.log 2> /var/www/vd.err &
 fi
--- node_campus_check.sh
+++ node_campus_check.sh
@@ -2,7 +2,7 @@
 
 pid=`ps aux | grep python | grep vd.py | awk '{print $2}'`
 
-if [[ !pid ]]; then
+if [[ -z pid ]]; then
    cd /home/pi/camp-node/
    python vd.py 1> vd.log 2> vd.err &
 fi
