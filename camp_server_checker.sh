ca30451e642dd088a874e8c56776a4bae80897db
Nala Ginrut <nalaginrut@gmail.com>
2015-07-07 19:41:24 +0800

    added echo information

--- camp_server_checker.sh
+++ camp_server_checker.sh
@@ -3,5 +3,6 @@
 pid=`ps aux | grep guile | grep vd.scm | awk '{print $2}'`
 
 if [[ -z pid ]]; then
+   echo "vd.scm need to start!"
    exec guile /var/www/vd.scm 1> /var/www/vd.log 2> /var/www/vd.err &
 fi
--- node_campus_check.sh
+++ node_campus_check.sh
@@ -3,6 +3,7 @@
 pid=`ps aux | grep python | grep vd.py | awk '{print $2}'`
 
 if [[ -z pid ]]; then
+   echo "need to start!"
    cd /home/pi/camp-node/
    python vd.py 1> vd.log 2> vd.err &
 fi
