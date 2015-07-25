9ceb61bcc72a494f1f941c9655d33e541a6baea5
Nala Ginrut <nalaginrut@gmail.com>
2015-07-25 22:26:57 +0800

    fixed check bg and bg2 again

--- vd.py
+++ vd.py
@@ -160,7 +160,7 @@ def detect_notice_to_show():
 
 def update_bg():
     call("wget -o /dev/null '%s' -O bg2.jpg" % bg_url, shell=True)
-    if os.path.exists and not filecmp.cmp('bg.jpg', 'bg2.jpg'):
+    if os.path.exists('bg.jpg') and not filecmp.cmp('bg.jpg', 'bg2.jpg'):
        call("sudo killall fbi", shell=True)
        call("mv bg2.jpg bg.jpg", shell=True)
        call("sudo fbi -a -T 1 -noverbose bg.jpg", shell=True)
@@ -179,7 +179,7 @@ def main():
 
 def init():
     call("rm -fr bg.jpg bg2.jpg video/*", shell=True)
-    update_bg()
+    call("wget -o /dev/null '%s' -O bg.jpg" % bg_url, shell=True)
     call("sudo fbi -a -T 1 -noverbose bg.jpg", shell=True)
     get_json(register_url, "GET")
     signal.signal(signal.SIGALRM, heartbeat)
